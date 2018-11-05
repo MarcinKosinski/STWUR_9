ad1fe4ab3b188aff732053e95f336f6a18b7ccc1
MarcinKosinski <m.p.kosinski@gmail.com>
2018-11-05 18:50:30 +0100

    1.2

--- R/002_answers.R
+++ R/002_answers.R
@@ -33,24 +33,35 @@ transactional2 <-
 
 
 # Can below snippet can help with the survey data?
-
-survey %>% 
+survey2 <-
+  survey %>%
+  mutate(event = 
+           (subscribed =='Subscribed to this brand/company in the past, but cancelled my subscription to this brand/company') %>% as.integer) %>%
+  select(-subscribed) %>% 
+  mutate(time = ifelse(is.na(`How long did you keep your subscription before you cancelled?`),
+                          `How long ago did you begin your subscription?` %>% as.character() %>% gsub(' ago', '', x = .),
+                          `How long did you keep your subscription before you cancelled?` %>% as.character() %>% gsub(' ago', '', x = .)
+                          )) %>%
+  select(-c(1:2)) %>%
+  mutate(id = 1:nrow(.)) %>%
+  group_by(id) %>%
   mutate(
     time = case_when(
-      time == "Less than 5 months"     ~  ,
-      time == "Between 5 and 6 months" ~  , 
-      time == "Between 6 and 7 months" ~  ,
-      time == "Between 7 and 8 months" ~  ,
-      time == "Between 8 and 9 months" ~  ,
-      time == "Between 9 and 10 months" ~ ,
-      time == "Between 10 and 11 months" ~,
-      time == "Between 11 and 12 months" ~,
-      time == "Between 1 and 2 years" ~   ,
-      time == "Between 2 and 3 years" ~   , 
-      time == "Between 3 and 4 years" ~   ,
-      time == "Between 4 and 5 years" ~   
+      time == "Less than 5 months"     ~  runif(n = 1, min = 1, max = 150),
+      time == "Between 5 and 6 months" ~  runif(n = 1, min = 150, max = 180), 
+      time == "Between 6 and 7 months" ~  runif(n = 1, min = 180, max = 210),
+      time == "Between 7 and 8 months" ~  runif(n = 1, min = 210, max = 240),
+      time == "Between 8 and 9 months" ~  runif(n = 1, min = 240, max = 270),
+      time == "Between 9 and 10 months" ~ runif(n = 1, min = 270, max = 300),
+      time == "Between 10 and 11 months" ~runif(n = 1, min = 300, max = 330),
+      time == "Between 11 and 12 months" ~runif(n = 1, min = 330, max = 365),
+      time == "Between 1 and 2 years" ~   runif(n = 1, min = 365, max = 365*2),
+      time == "Between 2 and 3 years" ~   runif(n = 1, min = 365*2, max = 365*3), 
+      time == "Between 3 and 4 years" ~   runif(n = 1, min = 365*3, max = 365*4),
+      time == "Between 4 and 5 years" ~   runif(n = 1, min = 365*4, max = 365*5)
     )
-  )
+  ) %>% 
+  select(-id)
 
 # 1.2 Do every survey respondent can have different value of time?
 
